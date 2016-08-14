17bd20345f5ae2d5aed24c463de9087f5b954608
Igor Silva <igorjose15@gmail.com>
2016-08-13 23:18:16 -0300

    Controlador

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('GanhosCtrl', function($scope) {})
+.controller('GanhosCtrl', function($scope) {
+  $scope.ganhos = [{
+        nome: "Salario",
+        valor: 1000
+    }];
+    $scope.addGanho = function(e) {
+        $scope.ganhos.push({
+            nome: $scope.nome,
+            valor: $scope.valor
+        });
+        $scope.nome = "";
+        $scope.valor = 0;
+    };
+    $scope.getTotal = function() {
+        var rtnTotal = 0;
+        for (var i = 0; i < $scope.ganhos.length; i++) {
+            rtnTotal += $scope.ganhos[i].valor;
+        }
+        //Salva os ganhos
+        window.localStorage.setItem('ganhos', rtnTotal);
+        return rtnTotal;
+    };
+})
 
 .controller('DespesasCtrl', function($scope) {})
 
