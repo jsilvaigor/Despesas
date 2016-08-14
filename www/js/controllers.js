11d7451887ab9e8d9815de2dfae25e243e45c049
Igor Silva <igorjose15@gmail.com>
2016-08-13 23:32:56 -0300

    View e Controlador

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,12 +18,33 @@ angular.module('starter.controllers', [])
         for (var i = 0; i < $scope.ganhos.length; i++) {
             rtnTotal += $scope.ganhos[i].valor;
         }
-        
+
         return rtnTotal;
     };
 })
 
-.controller('DespesasCtrl', function($scope) {})
+.controller('DespesasCtrl', function($scope) {
+  $scope.despesas = [{
+        nome: "Compras",
+        valor: 1000
+    }];
+    $scope.addDespesa = function(e) {
+        $scope.despesas.push({
+            nome: $scope.nome,
+            valor: $scope.valor
+        });
+        $scope.nome = "";
+        $scope.valor = 0;
+    };
+    $scope.getTotal = function() {
+        var rtnTotal = 0;
+        for (var i = 0; i < $scope.despesas.length; i++) {
+            rtnTotal += $scope.despesas[i].valor;
+        }
+
+        return rtnTotal;
+    };
+})
 
 .controller('BalancoCtrl', function($scope) {})
 
