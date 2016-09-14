b2aff76eaa349a78abf9b184c8f41bcb03e4fda4
Igor Silva <igorjose15@gmail.com>
2016-09-14 14:34:12 -0300

    Adicionada autenticação com o Firebase

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,66 +1,89 @@
 angular.module('starter.controllers', [])
 
-.controller('GanhosCtrl', function($scope, $rootScope, firebaseData, $firebaseArray) {
-  $scope.ganhos = $firebaseArray(firebaseData.refGanhos());
-    $scope.addGanho = function(e) {
-        $scope.ganhos.$add({
-            nome: $scope.nome,
-            valor: $scope.valor
-        });
-        $scope.nome = "";
-        $scope.valor = 0;
+  .controller('GanhosCtrl', function ($scope, $rootScope, firebaseData, $firebaseArray) {
+    $scope.ganhos = $firebaseArray(firebaseData.refGanhos());
+    $scope.addGanho = function (e) {
+      $scope.ganhos.$add({
+        nome: $scope.nome,
+        valor: $scope.valor
+      });
+      $scope.nome = "";
+      $scope.valor = 0;
     };
-    $scope.getTotal = function() {
-        var rtnTotal = 0;
-        for (var i = 0; i < $scope.ganhos.length; i++) {
-            rtnTotal += $scope.ganhos[i].valor;
-        }
+    $scope.getTotal = function () {
+      var rtnTotal = 0;
+      for (var i = 0; i < $scope.ganhos.length; i++) {
+        rtnTotal += $scope.ganhos[ i ].valor;
+      }
 
-        $rootScope.totalGanhos = rtnTotal;
+      $rootScope.totalGanhos = rtnTotal;
 
-        return rtnTotal;
+      return rtnTotal;
     };
-})
+  })
 
-.controller('DespesasCtrl', function($scope, $rootScope, firebaseData, $firebaseArray) {
-  $scope.despesas = $firebaseArray(firebaseData.refDespesas());
-    $scope.addDespesa = function(e) {
-        $scope.despesas.$add({
-            nome: $scope.nome,
-            valor: $scope.valor
-        });
-        $scope.nome = "";
-        $scope.valor = 0;
+  .controller('DespesasCtrl', function ($scope, $rootScope, firebaseData, $firebaseArray) {
+    $scope.despesas = $firebaseArray(firebaseData.refDespesas());
+    $scope.addDespesa = function (e) {
+      $scope.despesas.$add({
+        nome: $scope.nome,
+        valor: $scope.valor
+      });
+      $scope.nome = "";
+      $scope.valor = 0;
     };
-    $scope.getTotal = function() {
-        var rtnTotal = 0;
-        for (var i = 0; i < $scope.despesas.length; i++) {
-            rtnTotal += $scope.despesas[i].valor;
-        }
+    $scope.getTotal = function () {
+      var rtnTotal = 0;
+      for (var i = 0; i < $scope.despesas.length; i++) {
+        rtnTotal += $scope.despesas[ i ].valor;
+      }
 
-        $rootScope.totalDespesas = rtnTotal;
+      $rootScope.totalDespesas = rtnTotal;
 
-        return rtnTotal;
+      return rtnTotal;
     };
-})
+  })
 
-.controller('BalancoCtrl', function($scope, $rootScope) {
+  .controller('BalancoCtrl', function ($scope, $rootScope) {
 
-  $scope.calculaSaude = function(){
-    var total = $rootScope.totalGanhos - $rootScope.totalDespesas;
+    $scope.calculaSaude = function () {
+      var total = $rootScope.totalGanhos - $rootScope.totalDespesas;
 
-    if(total >= 0){
-      $scope.saude = true;
-    }else{
-      $scope.saude = false;
+      if (total >= 0) {
+        $scope.saude = true;
+      } else {
+        $scope.saude = false;
+      }
+
+      return total;
+
     }
 
-    return total;
+  })
 
-  }
+  .controller('ContaCtrl', function ($scope) {
 
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        $scope.showLoginForm = false;
+        $scope.user = user;
+      } else {
+        $scope.showLoginForm = true;
+      }
+    });
 
+    $scope.login = function (email, senha) {
+      firebase.auth().signInWithEmailAndPassword(email, senha).catch(function (error) {
+        console.log(error);
+      });
+    };
 
-})
+    $scope.logout = function () {
+      firebase.auth().signOut().then(function () {
+        $scope.showLoginForm = true;
+      }, function (error) {
+        console.log(error);
+      })
+    }
 
-.controller('ContaCtrl', function($scope) {});
+  });
